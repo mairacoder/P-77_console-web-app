54d32c00a0055f6d689edf36ca5ba529a6b590ab
mairacoder <125368352+mairacoder@users.noreply.github.com>
2023-05-01 16:52:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("My first console project")
+   var fname="Yashika"
+   var lname="Rao"
+   var fullname=fname.concat(lname);
+   console.log(fullname)
+
+   var a=100;
+   var b=25;
+   var add=a+b;
+   console.log(add)
+   var sub=a-b;
+   console.log(sub)
+   var mup=a*b;
+   console.log(mup)
+   var dis=a/b;
+   console.log(dis)
